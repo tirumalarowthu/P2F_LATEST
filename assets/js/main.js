1116de04c9198b71d666df5f8979a69eea301209
tirumalarowthu <tirumalarowthuv@gmail.com>
2023-08-17 21:08:22 +0530

    Removed unnessary files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: BizPage
-* Updated: Jul 27 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
